f829f22c9af94796c148877a5d38abbb0e325f93
shadi-nakhal <lebdroid@gmail.com>
2020-10-21 15:51:59 +0300

    step 3

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ app.get('/', (req, res) => {
   res.send('ok')
 })
 
+app.get('/test', (req, res) => {
+    res.status(200).send('ok')
+    })
+  
+app.get('/time', (req, res) => {
+    let t = new Date();
+    res.status(200).send("<time>"+ t.getHours()+ ":" + (t.getSeconds() < 10 ? '0' : '') + t.getSeconds() + "</time>")
+    })
+
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`)
   })
-  
