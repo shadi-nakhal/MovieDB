e337c8dc882741e4abdc9f02086c46fac2959522
shadi-nakhal <lebdroid@gmail.com>
2020-10-21 22:39:41 +0300

    step 7

--- routes/movies.js
+++ routes/movies.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const movies = [
-    { title: 'Jaws', year: 1975, rating: 8 },
-    { title: 'Avatar', year: 2009, rating: 7.8 },
-    { title: 'Brazil', year: 1985, rating: 8 },
-    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+    { id: 1, title: 'Jaws', year: 1975, rating: 8 },
+    { id: 2, title: 'Avatar', year: 2009, rating: 7.8 },
+    { id: 3, title: 'Brazil', year: 1985, rating: 8 },
+    { id: 4, title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
 ];
 
 
@@ -21,19 +21,19 @@ router.get('/movies/read', function (req, res){
         })
     });
 
-router.get('/movies/read/:id', function (req, res){
+router.get('/movies/read/:sort', function (req, res){
     
-    if(req.params.id == "by-date"){
+    if(req.params.sort == "by-date"){
             res.send({
             status:200,
             data: movies.sort((a, b)=>{var dateA = new Date(a.year), dateB = new Date(b.year); return dateA - dateB;})
         })
-    }else if(req.params.id == "by-rating"){
+    }else if(req.params.sort == "by-rating"){
             res.send({
             status:200,
             data: movies.sort((a, b) => a.rating - b.rating)
         })
-    }else if(req.params.id == "by-title"){
+    }else if(req.params.sort == "by-title"){
             res.send({
             status:200,
             data: movies.sort((a, b)=>{
@@ -44,9 +44,25 @@ router.get('/movies/read/:id', function (req, res){
             })
         })
     }
-    
+
     });
-        
+
+router.get('/movies/read/id/:id', function (req, res){
+    let mov = movies.filter(item => Object.values(item).indexOf(Number(req.params.id)) == 0)
+    if(mov.length != 0){
+        res.send({
+            status:200,
+            data:mov
+        })
+    }else{
+        res.send({
+            status:404,
+            error:true,
+            message:"the movie with id " + req.params.id + " does not exist"
+        })
+    }
+})
+
 router.get('/movies/update', function (req, res){
     res.send("update")
     });
