9175df9554e0eff8845ce1d6df8a00c0ffba71d3
shadi-nakhal <lebdroid@gmail.com>
2020-10-21 20:29:52 +0300

    step 6

--- index.js
+++ index.js
@@ -5,19 +5,14 @@ const search = require('./routes/search');
 const app = express()
 const port = 3001
 
-const movies = [
-    { title: 'Jaws', year: 1975, rating: 8 },
-    { title: 'Avatar', year: 2009, rating: 7.8 },
-    { title: 'Brazil', year: 1985, rating: 8 },
-    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
-];
-
 app.get('/', (req, res) => {
   res.send('ok')
 });
 
 app.use(movies_router);
-app.use(fun_stuff);
+
+app.use(fun_stuff);  // the /hello/ /time/ /test/
+
 app.use(search);
 
 
--- routes/movies.js
+++ routes/movies.js
@@ -15,9 +15,36 @@ router.get('/movies/create', function (req, res){
     });
         
 router.get('/movies/read', function (req, res){
-    res.send({
+        res.send({
         status:200,
-        message: movies})
+        message: movies
+        })
+    });
+
+router.get('/movies/read/:id', function (req, res){
+    
+    if(req.params.id == "by-date"){
+            res.send({
+            status:200,
+            data: movies.sort((a, b)=>{var dateA = new Date(a.year), dateB = new Date(b.year); return dateA - dateB;})
+        })
+    }else if(req.params.id == "by-rating"){
+            res.send({
+            status:200,
+            data: movies.sort((a, b) => a.rating - b.rating)
+        })
+    }else if(req.params.id == "by-title"){
+            res.send({
+            status:200,
+            data: movies.sort((a, b)=>{
+                var titleA = a.title.toLowerCase(), titleB = b.title.toLowerCase();
+                if (titleA < titleB) return -1;
+                if (titleA > titleB) return 1;
+                return 0;
+            })
+        })
+    }
+    
     });
         
 router.get('/movies/update', function (req, res){
